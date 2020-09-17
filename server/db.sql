d4b9b7047d106b791925273411cac2aed658de93
JustinaJur <jurgeleviciute.justina@gmail.com>
2020-09-17 23:05:45 +0300

    chnage db structure, add some input validation

--- server/db.sql
+++ server/db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE cars
 
 CREATE TABLE cars_numbers(
-plate_number SERIAL PRIMARY KEY UNIQUE NOT NULL,
+plate_number VARCHAR(6) UNIQUE NOT NULL,
 owner_name VARCHAR(100) NOT NULL
-) 
+)
